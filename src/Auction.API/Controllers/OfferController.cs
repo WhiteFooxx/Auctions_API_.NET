3ce854a6db675ac431863a0c39721327af6a0702
Rodrigo Mufatto <rodrigom3317@gmail.com>
2024-02-08 22:15:02 -0400

    feat: create offer controller

--- src/Auction.API/Controllers/OfferController.cs
+++ src/Auction.API/Controllers/OfferController.cs
@@ -1,11 +1,22 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using RocketseatAuction.API.Communication.Requests;
+using RocketseatAuction.API.Filters;
+using RocketseatAuction.API.UseCases.Offers.CreateOffer;
 
 namespace RocketseatAuction.API.Controllers;
+
+[ServiceFilter(typeof(AuthenticationUserAttribute))]
 public class OfferController : RocketseatAuctionBaseController
 {
     [HttpPost]
-    public IActionResult CreateOffer()
+    [Route("{itemId}")]
+    public IActionResult CreateOffer(
+        [FromRoute]int itemId,
+        [FromBody] RequestCreateOfferJson request,
+        [FromServices] CreateOfferUseCase useCase)
     {
-        return Created();
+        var id = useCase.Execute(itemId, request);
+
+        return Created(string.Empty, id);
     }
 }
